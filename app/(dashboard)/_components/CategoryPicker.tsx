d231221be11f267f93f520047e2cc338491d6996
devhimanshuu <devhimanshuu@gmail.com>
2024-06-28 01:12:45 +0530

    CategoryPicker

---
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { TransactionType } from "@/lib/type";
+
+interface Props {
+  type: TransactionType;
+}
+import React from "react";
+
+const CategoryPicker = ({ type }: Props) => {
+  return <div></div>;
+};
+
+export default CategoryPicker;
